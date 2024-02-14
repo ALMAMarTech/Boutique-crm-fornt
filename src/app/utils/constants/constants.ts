4fe406f23ca70b23b6873d476a115cf3347bad89
Diego Alberto Vázquez Martínez <daniel.arias@almamartech.com>
2024-02-13 20:25:14 -0600

    commit ng buid

--- src/app/utils/constants/constants.ts
+++ src/app/utils/constants/constants.ts
@@ -1,7 +1,17 @@
-import {environment} from "../../../environments/environment";
+import { environment } from '../../../environments/environment';
 
+export class Constants {
+  public static HOST = 'http://localhost:8080';
+  public static APPNAME = environment.appName;
+  public static APPPASSWORD = environment.appPassword;
+}
+
+
+/*
 export class Constants {
   public static HOST = environment.host;
   public static APPNAME = environment.appName;
   public static APPPASSWORD = environment.appPassword;
 }
+ */
+
